b7cc2aa41c029e6887dbfcea261353246b8b28f1
euw2 <127389834+euw2@users.noreply.github.com>
2023-03-09 11:57:50 +0100

    Delete m.java

--- Lab1/m.java
+++
@@ -1,6 +0,0 @@
-public class m {
-    public static void main(String[] args) {
-        Segment seg = new Segment(new Point(0,0),new Point(10,10));
-        Segment arr[2]= Segment.perpendicular()
-    }
-}
